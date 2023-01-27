2de556d51b574caddae69bf2ec72cb54a35e9e28
Siphamandla Malaza <66687699+DaveTheBomb@users.noreply.github.com>
2023-01-27 09:45:09 +0200

    Decryption implementation

---
+++ Decryption.m
@@ -0,0 +1,5 @@
+function result =  Decryption(hexPlaintext, reversedHexadecimalKeys)
+    %result = Encryption(hexPlaintext, reversedBinaryKeys, reversedHexadecimalKeys);
+
+    result = Encryption(hexPlaintext, reversedHexadecimalKeys);
+end
